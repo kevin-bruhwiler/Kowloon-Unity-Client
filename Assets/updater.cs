24da2b4720363781625b5c2a67f7aec7840dc0eb
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2020-08-31 22:42:30 -0600

    added working teleportation (finally :P)

---
+++ Assets/updater.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updater : MonoBehaviour
+{
+
+    public VRTeleporter teleporter;
+    public OVRPlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.GetComponent<CharacterController>().enabled = true;
+        if (OVRInput.Get(OVRInput.Button.Three))
+        {
+            teleporter.ToggleDisplay(true);
+        }
+        if (OVRInput.GetUp(OVRInput.Button.Three))
+        {
+            player.GetComponent<CharacterController>().enabled = false;
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+    }
+}
