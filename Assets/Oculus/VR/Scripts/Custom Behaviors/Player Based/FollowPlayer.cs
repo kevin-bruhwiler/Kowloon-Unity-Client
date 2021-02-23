0f9a2e1c69878ed6ef9e86fd8d5f3e810b50cbaa
kevin-bruhwiler <kevin.bruhwiler@gmail.com>
2021-02-22 21:22:58 -0700

    custom behavior to follow player at moderate distance

---
+++ Assets/Oculus/VR/Scripts/Custom Behaviors/Player Based/FollowPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    Transform player;
+    float f_RotSpeed = 2.0f;
+    float f_MoveSpeed = 1.0f;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.position - transform.position), f_RotSpeed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, player.position) > 2)
+        {
+            transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+        }
+    }
+}
